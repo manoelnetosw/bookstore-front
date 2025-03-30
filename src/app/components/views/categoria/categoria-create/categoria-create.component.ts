c2a13645975b931cb7285c88ed8699c38eddc7ee
manoelnetosw <manoelnetosw@gmail.com>
2025-03-29 21:32:29 -0400

    Adicionado tela nova categoria e seus botoes

--- src/app/components/views/categoria/categoria-create/categoria-create.component.ts
+++ src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { CategoriaService } from '../categoria.service';
+import { Categoria } from '../categoria.model';
 
 @Component({
   selector: 'app-categoria-create',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './categoria-create.component.html',
   styleUrl: './categoria-create.component.css'
 })
 export class CategoriaCreateComponent {
+
+  categoria: Categoria = {
+    nome: '',
+    descricao: ''
+  };
+
+  constructor(private service: CategoriaService, private router: Router) { }
+
+  ngOnInit(): void { }
+
+  create(): void {
+    this.service.create(this.categoria).subscribe(resposta => {
+      this.service.mensagem('Categoria criada com sucesso!');
+      this.navegarParaCategoriaRead();
+    }, error => {
+      for (let i = 0; i < error.error.errors.length; i++) {
+        this.service.mensagem(error.error.errors[i].message);
+      }
+    });
+  }
+
+  navegarParaCategoriaRead(): void {
+    this.router.navigate(["categorias"]);
+  }
 
 }
--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Categoria } from './categoria.model';
 import { environment } from '../../../../environments/environment';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,24 @@ export class CategoriaService {
 
   baseUrl: String = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
   findAll(): Observable<Categoria[]> {
     const url = `${this.baseUrl}categorias`;
     return this.http.get<Categoria[]>(url);
   }
+
+  create(categoria: Categoria): Observable<Categoria> {
+    const url = `${this.baseUrl}categorias`;
+    return this.http.post<Categoria>(url, categoria);
+  }
+
+  mensagem(msg: String): void {
+    this._snack.open(`${msg}`, 'OK', {
+      duration: 3000,
+      horizontalPosition: 'end',
+      verticalPosition: 'top'
+    });
+  }
+
 }
